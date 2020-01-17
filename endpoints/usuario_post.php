bfeff73b604b16dca3b455875d7c25e66e90f388
Rebeca Pacheco <rbkgarner@gmail.com>
2020-01-16 22:45:17 -0300

    validação de email e criacao de usuario

--- endpoints/usuario_post.php
+++ endpoints/usuario_post.php
@@ -2,17 +2,40 @@
 
 function api_usuario_post($request) {
 
-$response = array(
-  'nome' => 'Rebeca',
-  'email' => 'rebecagpacheco@outlook.com'
-);
+  $email = sanitize_email($request['email']);
+  $nome = sanitize_text_field($request['nome']);
+  $senha = sanitize_text_field($request['senha']);
+  $rua = sanitize_text_field($request['rua']);
+  $bairro = sanitize_text_field($request['bairro']);
+  $numero = sanitize_text_field($request['numero']);
+  $cidade = sanitize_text_field($request['cidade']);
+  $cep = sanitize_text_field($request['cep']);
+  $estado = sanitize_text_field($request['estado']);
+
+  $user_exists = username_exists($email);
+  $email_exists = email_exists($email);
+
+  if(!$user_exists && !$email_exists && $email && $senha) {
+    $user_id = wp_create_user($email, $senha, $email);
+
+    $response = array(
+      'ID' => $user_id,
+      'display_name' => $name,
+      'first_name' => $nome,
+      'role' => 'subscriber'
+    );
+    wp_update_user($response);
+  } else {
+    $response = new WP_Error('email', 'E-mail já cadastrado.', array('status' => 403));
+  }
+
   return rest_ensure_response($response);
 }
 
 function register_api_usuario_post() {
   register_rest_route('api', '/usuario', array(
   array(
-    'methods' => 'GET',
+    'methods' => WP_REST_Server::CREATABLE,
     'callback' => 'api_usuario_post',
   ),
   ));
